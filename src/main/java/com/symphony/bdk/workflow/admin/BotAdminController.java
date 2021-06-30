b82f434daca9d7b4d973660129096b6bb1efc9f9
symphony-soufiane <soufiane.aourinmouche@symphony.com>
2021-06-30 15:42:12 +0200

    PLAT-10316: Define workflow language v1 In this PR, a json schema file is added to verify workflow input validation following the specifications discussed in the related spike (PLAT-10316). One valid and one invalid workflow yaml file are added too as examples. In the Controller level, we expect user to send a yaml file and we validate it using json-schema-validator.

--- src/main/java/com/symphony/bdk/workflow/admin/BotAdminController.java
+++ src/main/java/com/symphony/bdk/workflow/admin/BotAdminController.java
@@ -5,11 +5,13 @@ import com.symphony.bdk.gen.api.model.V4MessageSent;
 import com.symphony.bdk.spring.events.RealTimeEvent;
 import com.symphony.bdk.workflow.engine.WorkflowBuilder;
 import com.symphony.bdk.workflow.swadl.Workflow;
+import com.symphony.bdk.workflow.validators.YamlValidator;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -19,14 +21,40 @@ import java.util.Base64;
 @Component
 public class BotAdminController {
 
-    @Autowired
-    private MessageService messageService;
+    private final MessageService messageService;
+    private final WorkflowBuilder workflowBuilder;
 
-    @Autowired
-    private WorkflowBuilder workflowBuilder;
+    public BotAdminController(MessageService messageService, WorkflowBuilder workflowBuilder) {
+        this.messageService = messageService;
+        this.workflowBuilder = workflowBuilder;
+    }
 
     @EventListener
-    public void onMessageSent(RealTimeEvent<V4MessageSent> event) throws IOException {
+    public void onMessageSent(RealTimeEvent<V4MessageSent> event)
+        throws IOException, ProcessingException {
+        // consider a message with an attachment as a workflow to run
+        if (!event.getSource().getMessage().getAttachments().isEmpty()) {
+            String streamId = event.getSource().getMessage().getStream().getStreamId();
+            String messageId = event.getSource().getMessage().getMessageId();
+            String attachmentId = event.getSource().getMessage().getAttachments().get(0).getId();
+
+            byte[] attachment = messageService.getAttachment(streamId, messageId, attachmentId);
+            byte[] decodedAttachment = Base64.getDecoder().decode(attachment);
+
+            ProcessingReport report = YamlValidator.validateYamlString(new String(decodedAttachment));
+
+            if (report.isSuccess()) {
+                messageService.send(streamId,
+                    "<messageML>Yaml file is <b>VALID.</b></messageML>");
+            } else {
+                messageService.send(streamId,
+                    "<messageML>Yaml file is <b>NOT VALID!</b></messageML>");
+            }
+
+        }
+    }
+
+    /*public void onMessageSent(RealTimeEvent<V4MessageSent> event) throws IOException {
         // consider a message with an attachment as a workflow to run
         if (!event.getSource().getMessage().getAttachments().isEmpty()) {
             String streamId = event.getSource().getMessage().getStream().getStreamId();
@@ -48,6 +76,6 @@ public class BotAdminController {
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory()
                 .configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true));
         return mapper.readValue(workflow, Workflow.class);
-    }
+    }*/
 
 }
---
+++ src/main/java/com/symphony/bdk/workflow/validators/YamlValidator.java
@@ -0,0 +1,100 @@
+package com.symphony.bdk.workflow.validators;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.File;
+import java.io.IOException;
+
+public class YamlValidator {
+
+  private static final Logger logger = LoggerFactory.getLogger(YamlValidator.class);
+  private static final String JSON_SCHEMA_FILE= "json-schema.json";
+  private static final ObjectMapper objectMapper = new ObjectMapper();
+  private static final ObjectMapper yamlReader = new ObjectMapper(new YAMLFactory());
+  private static final JsonSchemaFactory factory = JsonSchemaFactory.byDefault();
+
+  private YamlValidator() {}
+
+  public static ProcessingReport validateYamlFile(String yamlPath) throws IOException, ProcessingException {
+    final JsonNode schemaJson = objectMapper.readTree(
+        loadResourceWithClassPath(JSON_SCHEMA_FILE));
+    final JsonNode yamlProposalOne = convertYamlFileToJsonNode(yamlPath);
+
+    return validate(yamlProposalOne, schemaJson);
+  }
+
+  public static ProcessingReport validateYamlString(String yamlString) throws IOException, ProcessingException {
+    final JsonNode schemaJson = objectMapper.readTree(
+        loadResourceWithClassPath(JSON_SCHEMA_FILE));
+    final JsonNode yamlProposalOne = convertYamlStringToJsonNode(yamlString);
+
+    return validate(yamlProposalOne, schemaJson);
+  }
+
+  /**
+   * This method validates a {@link JsonNode} against a json schema
+   * @param jsonNode: json to be validated
+   * @param jsonSchema: schema to use for validation
+   * @return Report with success/failure status
+   */
+  private static ProcessingReport validate(JsonNode jsonNode, JsonNode jsonSchema)
+      throws ProcessingException {
+    final JsonSchema schema = factory.getJsonSchema(jsonSchema);
+    ProcessingReport report = schema.validate(jsonNode);
+
+    if (report.isSuccess()) {
+      logger.info("Yaml file VALID");
+    } else {
+      logger.info("Yaml file NOT VALID");
+    }
+
+    return report;
+  }
+
+  /**
+   * This method loads file using class path
+   * @param filename: relative path to the resource file
+   */
+  private static File loadResourceWithClassPath(String filename) throws IOException {
+    return new ClassPathResource(filename).getFile();
+  }
+
+  /**
+   * This method converts yaml file to {@link JsonNode}
+   * @param yamlFilename: relative path to yaml file
+   */
+  private static JsonNode convertYamlFileToJsonNode(String yamlFilename) throws IOException {
+    Object obj = yamlReader.readValue(
+        loadResourceWithClassPath(yamlFilename), Object.class);
+    return writeJsonNode(obj);
+  }
+
+  /**
+   * This method converts yaml string to {@link JsonNode}
+   * @param yamlString: Yaml content as a string
+   */
+  private static JsonNode convertYamlStringToJsonNode(String yamlString) throws IOException {
+    return writeJsonNode(yamlReader.readValue(yamlString, Object.class));
+
+  }
+
+  /**
+   * This method creates {@link JsonNode} from an {@link Object}
+   */
+  private static JsonNode writeJsonNode(Object obj)
+      throws JsonProcessingException {
+    String jsonString = objectMapper.writeValueAsString(obj);
+    return objectMapper.readTree(jsonString);
+  }
+
+}
