52c2e837b8153563190d8164acfc6134c770bca8
Mariacristina De Dominicis <65179248+symphony-mariacristina@users.noreply.github.com>
2021-10-27 14:45:02 +0200

    PLAT-11469 Adding message update activity (#57)
    * PLAT-11469 Adding message update activity
    
    Goal of this ticket is to add support of the Message Update feature. A new activity is been added which follows the following format:
      - update-message:
          id: updateMessage
          message-id: messageId
          content: Message Updated

---
+++ workflow-bot-app/src/main/java/com/symphony/bdk/workflow/engine/executor/message/UpdateMessageExecutor.java
@@ -0,0 +1,26 @@
+package com.symphony.bdk.workflow.engine.executor.message;
+
+import com.symphony.bdk.core.service.message.model.Message;
+import com.symphony.bdk.gen.api.model.V4Message;
+import com.symphony.bdk.workflow.engine.executor.ActivityExecutor;
+import com.symphony.bdk.workflow.engine.executor.ActivityExecutorContext;
+import com.symphony.bdk.workflow.swadl.v1.activity.message.UpdateMessage;
+
+import java.io.IOException;
+
+import static com.symphony.bdk.workflow.engine.executor.message.SendMessageExecutor.OUTPUT_MESSAGE_ID_KEY;
+import static com.symphony.bdk.workflow.engine.executor.message.SendMessageExecutor.OUTPUT_MESSAGE_KEY;
+
+public class UpdateMessageExecutor implements ActivityExecutor<UpdateMessage> {
+
+  @Override
+  public void execute(ActivityExecutorContext<UpdateMessage> execution) throws IOException {
+    String messageId = execution.getActivity().getMessageId();
+    V4Message messageToUpdate =  execution.bdk().messages().getMessage(messageId);
+    Message message = Message.builder().content(execution.getActivity().getContent()).build();
+    V4Message updatedMessage = execution.bdk().messages().update(messageToUpdate, message);
+
+    execution.setOutputVariable(OUTPUT_MESSAGE_KEY, updatedMessage);
+    execution.setOutputVariable(OUTPUT_MESSAGE_ID_KEY, updatedMessage.getMessageId());
+  }
+}
