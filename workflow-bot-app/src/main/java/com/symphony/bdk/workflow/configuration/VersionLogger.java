8a3d64d551841333415ec58021f270bb0e94ff68
Soufiane Aourinmouche <52406574+symphony-soufiane@users.noreply.github.com>
2022-03-23 16:24:50 +0100

    PLAT-11870: Prepare GA release (#96)
    Added a log of wdk version using Gradle resource filtering. A placehoder is added in spring application.yaml file that is replaced after the build with the current version. User-Agent header property is also handled in the same way.
    Removed a disclaimer from README.

---
+++ workflow-bot-app/src/main/java/com/symphony/bdk/workflow/configuration/VersionLogger.java
@@ -0,0 +1,23 @@
+package com.symphony.bdk.workflow.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
+public class VersionLogger {
+
+  @Value("${version}")
+  private String version;
+
+  @PostConstruct
+  private void logWdkVersion() {
+    if (!StringUtils.isBlank(version)) {
+      log.info("Running with WDK version: {}", version);
+    }
+  }
+}
