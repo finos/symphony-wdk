8f2caaac25762dcc5c224fbf90147925e0e40215
symphony-soufiane <soufiane.aourinmouche@symphony.com>
2022-09-15 11:13:39 +0200

    Get ride of ExceptionHandler and keep using GlobalExceptionHandler

--- workflow-bot-app/src/main/java/com/symphony/bdk/workflow/exception/GlobalExceptionHandler.java
+++ workflow-bot-app/src/main/java/com/symphony/bdk/workflow/exception/GlobalExceptionHandler.java
@@ -5,9 +5,14 @@ import com.symphony.bdk.workflow.api.v1.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+
+@Component
+@ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
