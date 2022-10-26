648d71fc35b0f46edba7a6bfff0d4fed387904ac
Soufiane Aourinmouche <52406574+symphony-soufiane@users.noreply.github.com>
2022-10-26 11:48:50 +0200

    Add optional id for events (#178)
    Added optional id for events in SWADL.
    This id makes events unique and will allow to store them in database variables.

--- workflow-language/src/main/java/com/symphony/bdk/workflow/swadl/v1/event/FormRepliedEvent.java
+++ workflow-language/src/main/java/com/symphony/bdk/workflow/swadl/v1/event/FormRepliedEvent.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-public class FormRepliedEvent {
+public class FormRepliedEvent extends InnerEvent {
   @JsonProperty
   private String formId;
 
