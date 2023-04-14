008907ea0689aae7cd42427b08b380743640c172
symphony-soufiane <soufiane.aourinmouche@symphony.com>
2023-04-14 10:29:45 +0200

    Add exceptions stacktrace to the log streaming endpoint. The log streaming appender was only adding exceptions formatted message. As the stacktrace can help troubleshooting, we added the stacktrace lines to the streamed payload.

--- workflow-bot-app/src/main/java/com/symphony/bdk/workflow/logs/LogsStreamingAppender.java
+++ workflow-bot-app/src/main/java/com/symphony/bdk/workflow/logs/LogsStreamingAppender.java
@@ -1,11 +1,15 @@
 package com.symphony.bdk.workflow.logs;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.StackTraceElementProxy;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.SmartLifecycle;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -16,8 +20,19 @@ public class LogsStreamingAppender extends UnsynchronizedAppenderBase<ILoggingEv
 
   @Override
   protected void append(ILoggingEvent eventObject) {
+    StringBuilder formattedMessage = new StringBuilder();
+    formattedMessage.append(eventObject.getFormattedMessage());
+
+    if (eventObject.getThrowableProxy() != null) {
+      formattedMessage.append("\n")
+              .append(Arrays.stream(eventObject.getThrowableProxy().getStackTraceElementProxyArray())
+                      .map(StackTraceElementProxy::getSTEAsString)
+                      .collect(Collectors.joining("\n")));
+    }
+
+
     service.broadcast(eventObject.getTimeStamp(), eventObject.getLevel().toString(), eventObject.getLoggerName(),
-        eventObject.getFormattedMessage());
+            formattedMessage.toString());
   }
 
   @Override
