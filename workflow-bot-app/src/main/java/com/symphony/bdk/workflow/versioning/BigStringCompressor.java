1e22a1ebb943a879df1f07c596716aee8829d545
Yinan Liu <60390925+yinan-symphony@users.noreply.github.com>
2023-04-21 09:29:44 +0200

    Compress the swadl string to save storage (#257)
    Add Spring Jpa attribute converter to compress and decompress
    the swadl attribute string, in order to save storage.

---
+++ workflow-bot-app/src/main/java/com/symphony/bdk/workflow/versioning/BigStringCompressor.java
@@ -0,0 +1,53 @@
+package com.symphony.bdk.workflow.versioning;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class BigStringCompressor implements AttributeConverter<String, byte[]> {
+  @Override
+  public byte[] convertToDatabaseColumn(String attribute) {
+    try {
+      return compress(attribute);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static byte[] compress(String attribute) throws IOException {
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    GZIPOutputStream gzipOutputStream = new GZIPOutputStream(outputStream);
+    gzipOutputStream.write(attribute.getBytes(StandardCharsets.UTF_8));
+    gzipOutputStream.close();
+    return outputStream.toByteArray();
+  }
+
+  @Override
+  public String convertToEntityAttribute(byte[] dbData) {
+    try {
+      return decompress(dbData);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static String decompress(byte[] dbData) throws IOException {
+    ByteArrayInputStream inputStream = new ByteArrayInputStream(dbData);
+    GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
+    byte[] buffer = new byte[100];
+    int length;
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    while ((length = gzipInputStream.read(buffer)) > 0) {
+      outputStream.write(buffer, 0, length);
+    }
+    gzipInputStream.close();
+    outputStream.close();
+    return outputStream.toString(StandardCharsets.UTF_8);
+  }
+}
